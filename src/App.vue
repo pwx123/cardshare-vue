7d9f5c275fb8c70745e34bc6f1e980fc4240e39a
pwx123 <1813536955@qq.com>
2018-10-14 21:46:52 +0800

    增加github地址

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
       <router-view/>
     </keep-alive>
     <tab v-if="!$route.meta.fullScreen" />
+    <github id="github"/>
   </div>
 </template>
 
 <script>
 import tab from "@/components/Tab/Tab";
+import github from '@/base/github/github';
 
 export default {
   components: {
-    tab
+    tab,
+    github
   }
 };
 </script>
@@ -21,4 +24,9 @@ export default {
 html, body, #app
   width 100%
   height 100%
+#github
+  position fixed
+  top 0
+  right 60px
+  z-index 999
 </style>
